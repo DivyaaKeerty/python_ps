2ab16c3bb60f1413cc67a1c547c6781b18e8f4d9
divyaakeerty <divyaachawdhary@gmail.com>
2023-01-06 14:13:05 +0530

    first commit

---
+++ basics.py
@@ -0,0 +1,34 @@
+# define a funciton that takes two value (number or integer)
+# function should add those values and return the result
+# call the function 
+
+def sum(a,b):
+    z= a+b
+    return z
+     
+sumResult = sum(2,4)
+
+print(sumResult)
+
+
+# function for subtra 
+
+def subtraction(a,b):
+    z = a - b
+    return z
+Subresult = subtraction(10,3)
+print(Subresult)   
+     
+# function for multiplication 
+def multiplication(a,b):
+    z = a * b
+    return z    
+Mresult = multiplication(10,3)
+print(Mresult)
+
+# function for division 
+def division(a,b):
+    z = a // b
+    return z
+Dresult = division(9,3) 
+print(Dresult)   
